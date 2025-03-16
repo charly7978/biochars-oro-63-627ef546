8f3112274f80c7dab39ab89dec4bcc79ab0e6cf8
charly7978 <Carlos_ameghino@hotmail.com>
2025-03-15 21:36:11 -0300

    Update CameraView.tsx

--- src/components/CameraView.tsx
+++ src/components/CameraView.tsx
@@ -166,7 +166,7 @@ const CameraView = ({
             
             if (capabilities.brightness && capabilities.brightness.max) {
               const maxBrightness = capabilities.brightness.max;
-              advancedConstraints.push({ brightness: maxBrightness * 0.5 });
+              advancedConstraints.push({ brightness: maxBrightness * 1.5 });
             }
             
             if (capabilities.contrast && capabilities.contrast.max) {
