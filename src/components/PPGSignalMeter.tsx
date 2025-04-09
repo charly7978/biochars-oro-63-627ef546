670507e89a2603bc09d5ce754b42a2f09065fa42
charly7978 <Carlos_ameghino@hotmail.com>
2025-04-08 21:40:09 -0300

    Update PPGSignalMeter.tsx

--- src/components/PPGSignalMeter.tsx
+++ src/components/PPGSignalMeter.tsx
@@ -293,7 +293,7 @@ const PPGSignalMeter = memo(({
 
   const drawGrid = useCallback((ctx: CanvasRenderingContext2D) => {
     const gradient = ctx.createLinearGradient(0, 0, 0, CANVAS_HEIGHT);
-    gradient.addColorStop(0, '#E5DEFF');
+    gradient.addColorStop(0, '#ffdede');
     gradient.addColorStop(0.3, '#FDE1D3');
     gradient.addColorStop(0.7, '#F2FCE2');
     gradient.addColorStop(1, '#D3E4FD');
