735133757e9cd9d4904eb07165ce763d7533cea2
charly7978 <Carlos_ameghino@hotmail.com>
2025-05-03 18:48:04 -0300

    Update index.ts

--- src/services/arrhythmia/index.ts
+++ src/services/arrhythmia/index.ts
@@ -1,8 +1,12 @@
-
-import ArrhythmiaDetectionService from './ArrhythmiaDetectionService';
+// Re-export the class itself as named export
+export { ArrhythmiaDetectionService } from './ArrhythmiaDetectionService';
+// Re-export other relevant types/utils
 export * from './types';
 export * from './utils';
 export * from './constants';
 export { ArrhythmiaWindowManager } from './ArrhythmiaWindowManager';
-export { ArrhythmiaDetectionService };
-export default ArrhythmiaDetectionService;
+
+// Get the singleton instance and export it as default
+import { ArrhythmiaDetectionService as ServiceClass } from './ArrhythmiaDetectionService';
+const ArrhythmiaDetectionServiceInstance = ServiceClass.getInstance();
+export default ArrhythmiaDetectionServiceInstance;
